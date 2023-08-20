510a400f070cb4bdafd158af4d3c240e0050f848
Prathmesh092001 <prathmesh.22msd7035@vitapstudent.ac.in>
2023-08-20 21:46:07 +0530

    prepare callbacks added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
+
+
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -11,8 +13,6 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-
-
 
 
 
